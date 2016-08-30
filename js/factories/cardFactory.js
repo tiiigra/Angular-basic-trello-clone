e3573fe1a020da2b2d92fba5e4b8dbc98b20ef39
Tatiana Baklan <tiigra@linux-51p0.suse>
2016-08-30 16:32:00 +0300

    Add 'Edit card' functionality

--- js/factories/cardFactory.js
+++ js/factories/cardFactory.js
@@ -1,3 +1,44 @@
-/**
- * Created by tiigra on 8/30/16.
- */
+angular.module('app').factory('cardFactory', function(){
+    var service = {};
+
+    var cards =[
+        {
+            id: 1,
+            description: "Fix bug in player",
+            list_id: 1
+        },
+        {
+            id: 2,
+            description: "Add d3 feature",
+            list_id: 1
+        },
+        {
+            id: 3,
+            description: "Learn AngularJS",
+            list_id: 3
+        }
+    ];
+
+    service.getCards = function (list) {
+        return _.filter(cards, {list_id: list.id});
+    };
+
+    service.createCard = function(list, cardDescription) {
+        cards.push({
+            id: _.uniqueId('card_'),
+            description: cardDescription,
+            list_id: list.id
+        });
+    };
+    
+    service.deleteCard = function (card) {
+        return _.pull(cards, card);
+    };
+    
+    service.updateCard = function (updatingCard) {
+        var card = _.find(cards, {id: updatingCard.id});
+        card.description = updatingCard.description;
+    };
+
+    return service;
+});
